6299982875d83343a72bc369247aa84d0fe0803e
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-03-23 18:26:01 +0100

    debugging the service request checkin and checkout

--- app/Http/Controllers/CheckInOutController.php
+++ app/Http/Controllers/CheckInOutController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\CheckIn;
 use App\Models\SubmittedQuotes;
 use Illuminate\Http\Request;
+use App\Notifications\CustomNotification;
 
 class CheckInOutController extends Controller
 {
