4ea144b527d3d0cfa9ee7a732cff0402b6bbaed8
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-03-23 20:41:19 +0100

    updated from Close Request to Closed - for final service request status

--- app/Http/Controllers/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequestController.php
@@ -286,7 +286,7 @@ class ServiceRequestController extends Controller
                 DB::beginTransaction();
                 try {
                     $serviceRequest = ServiceRequestModel::findOrFail($requestId);
-                    $apportionment = $this->apportionment($serviceRequest); // Corrected method name
+                    $apportionment = $this->aportionment($serviceRequest); // Corrected method name
 
                     // Create PaymentApportionment record
                     PaymentApportionment::create([
