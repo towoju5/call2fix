f59fe3d22d5939edb87a7fbd25340c56181e53ba
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-04-07 11:25:40 +0100

    Custom Notification class imported into DojahWebhook

--- app/Http/Controllers/VerificationWebhookController.php
+++ app/Http/Controllers/VerificationWebhookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Notifications\CustomNotification;
 use Illuminate\Support\Facades\Log;
 
 class VerificationWebhookController extends Controller
