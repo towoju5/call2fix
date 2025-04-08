c8a697ff4be698cb6fddd667c2142bc79e5fd281
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-04-08 08:49:34 +0100

    removed limit on

--- app/Models/BusinessOfficeAddress.php
+++ app/Models/BusinessOfficeAddress.php
@@ -21,12 +21,13 @@ class BusinessOfficeAddress extends Model
 
     protected static function booted()
     {
-        // $subscription = $this->user()->activeSubscription();
-        // $allowedOfficeAddresses = $subscription->getRemainingOf('locations');
+        $user = auth()->user();
+        $subscription = $user->activeSubscription();
+        $allowedOfficeAddresses = $subscription->getRemainingOf('locations');
         // Automatically apply a global scope to only return active records
         static::addGlobalScope('active', function (Builder $builder) use ($allowedOfficeAddresses) {
             $builder->where('is_active', true);
-            // $builder->limit($allowedOfficeAddresses);
+            $builder->limit($allowedOfficeAddresses);
         });
     }
 
