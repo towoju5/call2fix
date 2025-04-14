e94605c2b0f0c2a6126dd3bb69372c2bd8d6b2d2
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-04-14 17:59:36 +0100

    business_info

--- Modules/Artisan/app/Http/Controllers/TaskController.php
+++ Modules/Artisan/app/Http/Controllers/TaskController.php
@@ -15,7 +15,7 @@ class TaskController extends Controller
     {
         try {
             $tasks = ServiceRequestModel::latest()->where('request_status', 'Work In Progress')->orWhere('request_status', 'Awaiting Approval')
-                        // ->where('approved_artisan_id', auth()->id())
+                        ->where('approved_artisan_id', auth()->id())
                         ->with('service_provider', 'serviceCategory', 'submittedQuotes')->get();
             return get_success_response($tasks, "All tasks retrieved successfully");
         } catch (\Throwable $th) {
